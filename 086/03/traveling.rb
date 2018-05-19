e8b187cb9722c05a272bd33f8dd257ffe376bf0d
Leon <leopard10000@gmail.com>
2018-05-19 09:18:22 +0900

    ABC 086-C Traveling: 入力を最後までgetsするようにした

--- 086/03/traveling.rb
+++ 086/03/traveling.rb
@@ -4,9 +4,14 @@ n = gets.to_i
 lx = ly = 0
 # 前に存在していた点にいた時刻
 lt = 0
+# 合否
+is_enable = true
 
 n.times do
   t, x, y = gets.split.map(&:to_i)
+  if !is_enable
+    next  # 既にNGだとわかっているので読み飛ばすだけ ※最後まで読み込まないとAtCoderではRuntimeErrorになる
+  end
 
   # 前に存在していた点からの距離（道のり）
   distance = (x - lx).abs + (y - ly).abs
@@ -15,14 +20,14 @@ n.times do
   dt = t - lt
 
   if dt < distance
-    puts 'No'
-    exit 1
+    is_enable = false
+    next
   end
 
   # 経過時刻が偶数なら距離も偶数じゃなければならない。奇数でも同様。
   if dt.even? != distance.even?
-    puts 'No'
-    exit 1
+    is_enable = false
+    next
   end
 
   lt = t
@@ -30,4 +35,4 @@ n.times do
   ly = y
 end
 
-puts 'Yes'
+puts (is_enable ? 'Yes' : 'No')
