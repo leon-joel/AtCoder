749c7d300c7e2bb0b19d63aeb1f79954cf478544
Leon <leopard10000@gmail.com>
2018-05-19 09:06:38 +0900

    ABC 086-C Traveling: AtCoderではランタイムエラーになった！入力をgetsするのを途中でやめたらダメなようだ。

---
+++ 086/03/traveling.rb
@@ -0,0 +1,33 @@
+n = gets.to_i
+
+# 前に存在していた点
+lx = ly = 0
+# 前に存在していた点にいた時刻
+lt = 0
+
+n.times do
+  t, x, y = gets.split.map(&:to_i)
+
+  # 前に存在していた点からの距離（道のり）
+  distance = (x - lx).abs + (y - ly).abs
+
+  # 前に存在していた点にいた時刻からの経過時刻
+  dt = t - lt
+
+  if dt < distance
+    puts 'No'
+    exit 1
+  end
+
+  # 経過時刻が偶数なら距離も偶数じゃなければならない。奇数でも同様。
+  if dt.even? != distance.even?
+    puts 'No'
+    exit 1
+  end
+
+  lt = t
+  lx = x
+  ly = y
+end
+
+puts 'Yes'
