1e751476d0d3c2b9fc81f8f2c9b17fad4b21f769
Leon <leopard10000@gmail.com>
2018-05-26 19:37:07 +0900

    ABC 065-B: Trained? : ループ状態の問題。ARGFから直接mapを噛ましてArrayを作るようにした。

--- 065/B/main.rb
+++ 065/B/main.rb
@@ -1,25 +1,29 @@
 def main
   n = ARGF.gets.to_i
 
-  buttons = []
-  n.times do |i|
-    buttons << ARGF.gets.to_i - 1
-  end
+  buttons = ARGF.map(&:to_i)
 
   current = 0
   cnt = 1
   loop do
-    next_num = buttons[current]
+    next_num = buttons[current] - 1
     if next_num == 1
+      # 所望の2番ボタンが光った！
       puts cnt
       return
     elsif next_num == -1
+      # 既に押したボタンだった＝ループ状態に入った＝もうだめ
       puts "-1"
       return
     else
-      buttons[current] = -1
+      # 押したボタンは0にしておく
+      buttons[current] = 0
       cnt += 1
       current = next_num
+
+      #
+      # 0を入れていく方式ではなく、「N回トライしてみてだめだったらだめ」
+      # という方式が模範解答だった。
     end
   end
 
