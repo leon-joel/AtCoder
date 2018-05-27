33f1cefe9ac63c69b31219fd95277425e7d7bcfe
Leon <leopard10000@gmail.com>
2018-05-27 18:00:23 +0900

    ABC 079-C: TrainTicket: ベタ実装してみた。やっぱりこれが一番確実かな。

--- 079/C/main.rb
+++ 079/C/main.rb
@@ -1,33 +1,26 @@
-def calc(lhs, op, rhs)
-  if op == '+'
-    lhs + rhs
-  else
-    lhs - rhs
-  end
-end
-
 def main
-  nums = ARGF.gets.chars.map(&:to_i)
-  # puts nums
+  a, b, c, d = ARGF.gets.chars.map(&:to_i)
 
-  ['+', '-'].each do |op1|
-    ans1 = calc(nums[0], op1, nums[1])
-
-    ['+', '-'].each do |op2|
-      ans2 = calc(ans1, op2, nums[2])
-
-      ['+', '-'].each do |op3|
-        ans3 = calc(ans2, op3, nums[3])
-
-        if ans3 == 7
-          puts "#{nums[0]}#{op1}#{nums[1]}#{op2}#{nums[2]}#{op3}#{nums[3]}=7"
-          return
-        end
+  s = if a+b+c+d == 7
+        "#{a}+#{b}+#{c}+#{d}=7"
+      elsif a+b+c-d == 7
+        "#{a}+#{b}+#{c}-#{d}=7"
+      elsif a+b-c+d == 7
+        "#{a}+#{b}-#{c}+#{d}=7"
+      elsif a+b-c-d == 7
+        "#{a}+#{b}-#{c}-#{d}=7"
+      elsif a-b+c+d == 7
+        "#{a}-#{b}+#{c}+#{d}=7"
+      elsif a-b+c-d == 7
+        "#{a}-#{b}+#{c}-#{d}=7"
+      elsif a-b-c+d == 7
+        "#{a}-#{b}-#{c}+#{d}=7"
+      elsif a-b-c-d == 7
+        "#{a}-#{b}-#{c}-#{d}=7"
+      else
+        "error"
       end
-    end
-  end
-
-  puts 'error'  # 問題の前提上、ありえない
+  puts s
 end
 
 if __FILE__ == $0
