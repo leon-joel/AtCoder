ddb32fe57e1325276f14dde1a3f908405ac2f75e
Leon <leopard10000@gmail.com>
2018-05-27 18:02:38 +0900

    ABC 079-C: TrainTicket: ループで実装するなら、一番内側で計算するのが一番シンプル

--- 079/C/main.rb
+++ 079/C/main.rb
@@ -1,26 +1,30 @@
-def main
-  a, b, c, d = ARGF.gets.chars.map(&:to_i)
+def calc(lhs, op, rhs)
+  if op == '+'
+    lhs + rhs
+  else
+    lhs - rhs
+  end
+end
 
-  s = if a+b+c+d == 7
-        "#{a}+#{b}+#{c}+#{d}=7"
-      elsif a+b+c-d == 7
-        "#{a}+#{b}+#{c}-#{d}=7"
-      elsif a+b-c+d == 7
-        "#{a}+#{b}-#{c}+#{d}=7"
-      elsif a+b-c-d == 7
-        "#{a}+#{b}-#{c}-#{d}=7"
-      elsif a-b+c+d == 7
-        "#{a}-#{b}+#{c}+#{d}=7"
-      elsif a-b+c-d == 7
-        "#{a}-#{b}+#{c}-#{d}=7"
-      elsif a-b-c+d == 7
-        "#{a}-#{b}-#{c}+#{d}=7"
-      elsif a-b-c-d == 7
-        "#{a}-#{b}-#{c}-#{d}=7"
-      else
-        "error"
+def main
+  nums = ARGF.gets.chars.map(&:to_i)
+
+  ['+', '-'].each do |op1|
+    ['+', '-'].each do |op2|
+      ['+', '-'].each do |op3|
+        a = calc(nums[0], op1, nums[1])
+        a = calc(a, op2, nums[2])
+        a = calc(a, op3, nums[3])
+
+        if a == 7
+          puts "#{nums[0]}#{op1}#{nums[1]}#{op2}#{nums[2]}#{op3}#{nums[3]}=7"
+          return
+        end
       end
-  puts s
+    end
+  end
+
+  puts 'error'  # 問題の前提上、ありえない
 end
 
 if __FILE__ == $0
