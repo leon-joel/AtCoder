d9e3b817603d5260807978cf5d005578200bc948
Leon <leopard10000@gmail.com>
2018-05-27 10:15:39 +0900

    ABC 098: Bも実装 ※変に凝ったことをせず全検索で行けるときは全検索で実装すべき！という教訓。

--- 098/B/main.rb
+++ 098/B/main.rb
@@ -1,64 +1,18 @@
+require 'set'
+
 def main
   n = ARGF.gets.to_i
 
-  # ords = Array.new(26, 0)
-  # chars = ARGF.gets.chars.map do |c|
-  #   ord = c.ord - 97
-  #   ords[ord] += 1
-  # end
-  # kind_num = ords.count {|n| 1 <= n}
-  # # puts ords
-  # # puts kind_num
-  #
-  # common_kind_nums = 0
-  # founds = Array.new(26)
-  # nums = 0
-  # chars.each do |ord|
-  #   ords[ord] -= 1
-  #   if founds[ord].nil?
-  #     nums += 1
-  #     founds[ord] = 1
-  #   end
-  #   k = nums + ords.count {|n| 1 <= n}
-  #   if common_kind_nums < k
-  #     common_kind_nums = k
-  #   end
-  #
-  #   if common_kind_nums == kind_num
-  #     puts kind_num
-  #     return
-  #   end
-  # end
-  # puts common_kind_nums
+  chars = ARGF.gets.chars.to_a
 
-  ords = ARGF.gets.chars.map do |c|
-    ord = c.ord - 97
-  end
-
-  k1 = 0
-  founds = Array.new(26)
   max_common = 0
   0.upto(n-2) do |i|
-    c = ords[i]
-    if founds[c].nil?
-      k1 += 1
-      founds[c] = 1
-    end
+    s1 = Set.new(chars[0..i])
+    s2 = Set.new(chars[i+1..n-1])
 
-    f2 = founds
-    common = 0
-    (i+1).upto(n-1) do |i2|
-      c2 = ords[i2]
-      if f2[c2] == 1
-        common += 1
-        f2[c2] = nil
-      end
-    end
-
-    if max_common < common
-      max_common = common
-    end
+    max_common = [max_common, (s1 & s2).count].max
   end
+
   puts max_common
 end
 
