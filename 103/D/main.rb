6fba740c0e8903286127f8dd7951331eb5f10a90
Leon <leopard10000@gmail.com>
2018-07-22 19:56:32 +0900

    ABC 103: D問題 ソートしない解法 少し最適化（Arrayに無駄な情報を入れないようにした） ※ソートする解法と比べ 229ms -> 221ms, 12664kb->9852kb

--- 103/D/main.rb
+++ 103/D/main.rb
@@ -3,20 +3,27 @@ def main
 
   # ソートしない解法 ※islands_num分のArrayを最初から用意しておくだけ
   # ※ソートする解法と比べて 229ms -> 192ms と速くなったが、12664kb->15740kb とメモリー使用量は増えた
-  requests = Array.new(islands_num) {Array.new}
+  requests = Array.new(islands_num)
   req_num.times do
-    req = ARGF.gets.split.map(&:to_i)
+    a, b = ARGF.gets.split.map(&:to_i)
     # 配列indexは0始まりである点に注意
-    requests[req[1]-1] << req
+    if requests[b-1].nil?
+      requests[b-1] = [a]
+    else
+      requests[b-1] << a
+    end
   end
 
   # 切断した場所 例）1の場合、1-2 の間を切断したという意味
   last = 0
-  cnt = requests.inject(0) do |cnt, rs|
-    if 0 < rs.size
-      rs.each do |req|
-        if last < req[0]
-          last = req[1] - 1
+
+  # 配列のindexを b に変換するために with_index(1) により1始まりにしている
+  # ※それが必要ない場合には each_with_index のほうがいいかも。
+  cnt = requests.each.with_index(1).inject(0) do |cnt, (as, i)|
+    if !as.nil?
+      as.each do |a|
+        if last < a
+          last = i - 1
           cnt += 1
         end
       end
