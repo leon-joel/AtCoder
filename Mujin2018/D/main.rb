4c8a141574ddf109ff7e3fbe453cdda455a526c8
Leon <leopard10000@gmail.com>
2018-08-08 22:02:52 +0900

    Mujin Programming Challenge 2018: D問題: メモ化＋全探索 で実装 Gridをクラス化。速度は変わらず。

--- Mujin2018/D/main.rb
+++ Mujin2018/D/main.rb
@@ -16,14 +16,15 @@ def process(x, y)
   end
 
   # 常に x >= y となるように入れ替える
-  # x, y = y, x if x < y
+  x, y = y, x if x < y
   [x, y]
 end
 
 # grid: nil=未調査 1=OK  0:NG
 def gojohou(bx, by, grid)
-  x = bx
-  y = by
+  ox, oy = bx, by
+  ox, oy = oy, ox if ox < oy
+  x, y = ox, oy
   xy_ary = []
   loop do
     if x < 10 || y < 10
@@ -33,14 +34,15 @@ def gojohou(bx, by, grid)
 
     # ループした？
     if xy_ary.find_index([x, y])
+      grid.set(ox, oy, 1)
       return true
     end
 
     xy_ary << [x, y]
 
-    # すでに条件を満たすことがわかっている？
-    v = grid[x][y]
+    v = grid.get(x, y)
     if !v.nil?
+      # すでに条件を満たすことがわかっている？
       if v == 1
         return true
       end
@@ -53,8 +55,20 @@ def gojohou(bx, by, grid)
 
     x, y = process(x, y)
   end
-  # failed << org_xy
+  grid.set(ox, oy, 0)
   false
+end
+
+class Grid
+  def initialize(sx, sy)
+    @grid = Array.new(1000) { Array.new(1000) }
+  end
+  def set(x, y, value)
+    @grid[x][y] = value
+  end
+  def get(x, y)
+    @grid[x][y]
+  end
 end
 
 def main
@@ -63,15 +77,12 @@ def main
   cnt = 0
 
   # success, fail を表す grid map を作る
-  grid = Array.new(1000) { Array.new(1000) }
+  grid = Grid.new(1000, 1000)
   # 10未満は調べない ※lim_xがbeginより下の場合は1度も回らない
   10.upto(lim_y) do |y|
     10.upto(lim_x) do |x|
       if gojohou(x, y, grid)
-        grid[x][y] = 1
         cnt += 1
-      else
-        grid[x][y] = 0
       end
     end
   end
