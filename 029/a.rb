af3e3e3616da15f2a2c57e9f6337fd24c53ee30a
Leon <leopard10000@gmail.com>
2018-08-09 22:53:18 +0900

    ABC 029-A: BitDPで実装。
    ※昔のA問題は難しかった…

---
+++ 029/a.rb
@@ -0,0 +1,42 @@
+require 'pp'
+
+def main
+  num = ARGF.gets.to_i
+
+  meats = []
+  num.times do |i|
+    meats << ARGF.gets.to_i
+  end
+  # pp meats
+
+  total_time = meats.inject(0) do |sum, t|
+    sum + t
+  end
+  # pp total_time
+
+  min_sum = 200
+
+  # bit mask の生成
+  0.upto((1 << num) - 1) do |mask|
+    sum1 = 0
+    # 各bitを下から舐める
+    0.upto(num-1) do |i|
+      # ビットが立っている＝鉄板1で焼く
+      if mask[i] == 1
+        sum1 += meats[i]
+      end
+    end
+
+    # 鉄板2の消費時間
+    sum2 = total_time - sum1
+
+    sum = [sum1, sum2].max
+    min_sum = sum if sum < min_sum
+  end
+
+  puts min_sum
+end
+
+if __FILE__ == $0
+  main
+end
---
+++ 029/spec/a_spec.rb
@@ -0,0 +1,70 @@
+require 'rspec'
+require 'active_support/core_ext/kernel/reporting'  # to suppress warning
+require_relative "../a"
+
+shared_examples "test_main" do |input, expected|
+  it do
+    # stdin_org = $stdin
+    stdout_org = $stdout
+
+    # 警告 already initialized constant を抑止するため active_support の silence_warnings を使用している
+    Kernel::silence_warnings do
+      ARGF = StringIO.new(input)
+    end
+    $stdout = StringIO.new
+    main
+    output = $stdout.string.chomp
+    # $stdin = stdin_org
+    $stdout = stdout_org
+
+    expect(output).to eq expected
+  end
+end
+
+describe "main_test0" do
+  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+4
+4
+6
+7
+10
+  EOS_INPUT
+14
+  EOS_EXPECTED
+end
+
+describe "main_test1" do
+  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+3
+1
+2
+4
+  EOS_INPUT
+4
+  EOS_EXPECTED
+end
+
+describe "main_test2" do
+  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+1
+29
+  EOS_INPUT
+29
+  EOS_EXPECTED
+end
+
+describe "main_test3" do
+  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+0
+  EOS_INPUT
+ABC
+  EOS_EXPECTED
+end
+
+# describe "main_test4" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2800
+#   EOS_INPUT
+# AGC
+#   EOS_EXPECTED
+# end
