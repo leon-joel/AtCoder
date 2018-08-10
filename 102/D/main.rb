117420790cafc918a4fe66198d15d0a92ad6abd7
Leon <leopard10000@gmail.com>
2018-08-10 19:15:25 +0900

    ABC 102-D: 真ん中固定のアイデア＋しゃくとり法 でAC

--- 102/D/main.rb
+++ 102/D/main.rb
@@ -1,29 +1,67 @@
+require 'pp'
+
 def main
-  n = ARGF.gets.chomp.to_i
+  nums_cnt = ARGF.gets.chomp.to_i
   nums = ARGF.gets.split.map(&:to_i)
 
-  sum = nums.inject(&:+)
+  # 累積和
+  s = 0
+  accum_ary = nums.map do |n|
+    s += n
+    s
+  end
+  # pp accum_ary
 
-  min_diff = 10**9
-  1.upto(nums.length - 3) do |c1|
-    r1_sum = nums.slice(0, c1).inject(&:+)
+  min_diff = nil
 
-    1.upto(nums.length - 2 - c1) do |c2|
-      r2_sum = nums.slice(c1, c2).inject(&:+)
+  # 2番目（真ん中）の区切りを決める
+  # m_idx=2 の場合、2の前が区切り という意味
+  l = 1
+  r = 3
+  2.upto(nums.length-2) do |m_idx|
+    # しゃくとり法
+    l_side_sum = accum_ary[m_idx-1] # left側の合計
 
-      1.upto(nums.length - 1 - c1 - c2) do |c3|
-        r3_sum = nums.slice(c1+c2, c3).inject(&:+)
-
-        r4_sum = sum - (r1_sum + r2_sum + r3_sum)
-
-        min, max = [r1_sum, r2_sum, r3_sum, r4_sum].minmax
-        d = max - min
-
-        if d < min_diff
-          min_diff = d
-        end
+    side_min_diff = nil
+    ll_sum = lr_sum = 0
+    # 左サイドの分割位置
+    l.upto(m_idx-1) do |i|
+      ll = accum_ary[i-1]
+      lr = l_side_sum - ll
+      diff = (ll - lr).abs
+      if side_min_diff.nil? || diff < side_min_diff
+        ll_sum, lr_sum = ll, lr
+        side_min_diff = diff
+        l = i
+      else
+        # 更新しなかった＝右端が行き過ぎた！
+        break
       end
     end
+
+    # しゃくとり法
+    r_side_sum = accum_ary[-1] - l_side_sum # right側の合計
+
+    side_min_diff = nil
+    rl_sum = rr_sum = 0
+    # 右サイドの分割位置
+    r.upto(nums.length-1) do |i|
+      rl = accum_ary[i-1] - l_side_sum
+      rr = r_side_sum - rl
+      diff = (rl - rr).abs
+      if side_min_diff.nil? || diff < side_min_diff
+        rl_sum, rr_sum = rl, rr
+        side_min_diff = diff
+        r = i
+      else
+        # 更新しなかった＝右端が行き過ぎた！
+        break
+      end
+    end
+
+    min, max = [ll_sum, lr_sum, rl_sum, rr_sum].minmax
+    diff = max - min
+    min_diff = diff if min_diff.nil? || diff < min_diff
   end
 
   puts min_diff
