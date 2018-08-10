1db5e4dbc7f5fa74ac4ed5aa7b917b287d10f54b
Leon <leopard10000@gmail.com>
2018-08-10 23:31:03 +0900

    AOJ DSL_3: しゃくとり法で実装 https://qiita.com/drken/items/ecd1a472d3a0e7db8dce

--- AOJ_DSL_3_C/main.rb
+++ AOJ_DSL_3_C/main.rb
@@ -1,4 +1,6 @@
-require 'pp'
+# しゃくとり法
+# https://qiita.com/drken/items/ecd1a472d3a0e7db8dce
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_C&lang=jp
 
 def main
   nums_len, q_len = ARGF.gets.split.map(&:to_i)
@@ -10,18 +12,29 @@ def main
     ans = 0
 
     # 左端からスタート
+    r = 0
+    sum = 0
     0.upto(nums.length-1) do |l|
       # [left, right) の総和が q 以下となる最大の right を求める
-      r = l
-      sum = 0
       while r < nums.length && sum + nums[r] <= q
         sum += nums[r]
         r += 1
       end
 
-      # この時点で r は(条件を満たす最大のidx)+1となっているので、
+      # この時点で r は(条件を満たす最大のidx)+1となっている。
+      # ※r側を開区間と考えると、rは（条件を満たす最大のidx）になっている。
       # (r - l) が可能な組み合わせ数
       ans += r - l
+
+      # leftを動かす準備
+      if r == l
+        # r と l が同じ位置の場合、rを動かす必要がある。
+        # そして、sumはすでに0のはずなので減算しない
+        r += 1
+      else
+        # leftが右に動くので減算しておく
+        sum -= nums[l]
+      end
     end
 
     puts ans
