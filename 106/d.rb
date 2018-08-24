e140775b955417057ba25dbce058946a737c42d7
Leon <leopard10000@gmail.com>
2018-08-24 13:08:52 +0900

    ABC 106: D: 2次元累積和でAC ※解説放送をみて

--- 106/d.rb
+++ 106/d.rb
@@ -3,48 +3,33 @@ require 'pp'
 def gets; ARGF.gets; end
 
 def main
-  # prng = Random.new
-  # 100000.times do
-  #   print "#{prng.rand(1000000000)} "
-  # end
-  # exit
+  n, m, q = gets.split.map(&:to_i)
 
-  n, m = gets.split.map(&:to_i)
-
-  # ハッシュ: 累積mod -> その個数
-  accum_mod_hash = Hash.new(0)
-  accum_mod = 0
-  gets.split.each do |s|
-    accum_mod = (s.to_i + accum_mod) % m
-    accum_mod_hash[accum_mod] += 1
+  # 区間を2次元座標上にマッピングする
+  # range_map[L][R]
+  # ※l,r は1以上なので 0行/0列 は全て0のまま
+  range_map = Array.new(n+1) {Array.new(n+1, 0)}
+  m.times do
+    l, r = gets.split.map(&:to_i)
+    range_map[l][r] += 1
   end
-  # pp accum_mod_hash
 
-  ans = accum_mod_hash.inject(0) do |sum, (k, v)|
-    if k == 0
-      sum += v * (v+1) / 2
-    else
-      sum += (v-1) * (v) / 2
+  # 2次元累積和を求める
+  1.upto(n) do |l|
+    row_sum = 0
+    1.upto(n) do |r|
+      row_sum += range_map[l][r]
+      range_map[l][r] = range_map[l-1][r] + row_sum
     end
-    sum
   end
+  # pp range_map
 
-  puts ans
-
-  # ans = 0
-  # 0.upto(nums.length-1) do |i|
-  #   m0 = nums[i]
-  #
-  #   # 起点が0、つまりその箱だけで条件を満たしている
-  #   ans += 1 if m0 == 0
-  #
-  #   # 後に続く m0 と同じ値をカウントアップする
-  #   (i+1).upto(nums.length-1) do |j|
-  #     ans += 1 if nums[j] == m0
-  #   end
-  # end
-  #
-  # puts ans
+  # 問題解決
+  q.times do
+    p, q = gets.split.map(&:to_i)
+    ans = range_map[q][q] - range_map[p-1][q] - range_map[q][p-1] + range_map[p-1][p-1]
+    puts ans
+  end
 end
 
 if __FILE__ == $0
