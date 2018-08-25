88e012059c1db331ca9a83a86695d2163828088f
Leon <leopard10000@gmail.com>
2018-08-25 20:10:30 +0900

    ABC 105: C: -2進数

--- 105/spec/c_spec.rb
+++ 105/spec/c_spec.rb
@@ -23,120 +23,109 @@ end
 
 describe "main_test0" do
   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 700
-3 500
-5 800
+-9
   EOS_INPUT
-3
+1011
   EOS_EXPECTED
 end
 
 describe "main_test1" do
   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 2000
-3 500
-5 800
+123456789
   EOS_INPUT
-7
+11000101011001101110100010101
   EOS_EXPECTED
 end
 
 describe "main_test2" do
   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 400
-3 500
-5 800
+0
   EOS_INPUT
-2
+0
   EOS_EXPECTED
 end
 
 describe "main_test3" do
   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-5 25000
-20 1000
-40 1000
-50 1000
-30 1000
-1 1000
+-1
   EOS_INPUT
-66
+11
   EOS_EXPECTED
 end
 
-describe "main_test4" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-1 100
-1 100000
-  EOS_INPUT
-1
-  EOS_EXPECTED
-end
-
-describe "main_test5" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 1500
-5 1000
-3 500
-  EOS_INPUT
-5
-  EOS_EXPECTED
-end
-
-describe "main_test6" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 1000
-5 1000
-3 500
-  EOS_INPUT
-3
-  EOS_EXPECTED
-end
-
-describe "main_test7" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 1200
-5 1000
-3 500
-  EOS_INPUT
-4
-  EOS_EXPECTED
-end
-
-describe "main_test8" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 300
-5 1000
-3 500
-  EOS_INPUT
-2
-  EOS_EXPECTED
-end
-
-describe "main_test9" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-2 1600
-5 1000
-3 500
-  EOS_INPUT
-6
-  EOS_EXPECTED
-end
-
-describe "main_test10" do
-  it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
-10 5400
-5 1000
-3 500
-5 1000
-3 500
-5 1000
-3 500
-5 1000
-3 500
-5 1000
-3 500
-  EOS_INPUT
-5
-  EOS_EXPECTED
-end
+# describe "main_test4" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 1 100
+# 1 100000
+#   EOS_INPUT
+# 1
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test5" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2 1500
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 5
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test6" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2 1000
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 3
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test7" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2 1200
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 4
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test8" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2 300
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 2
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test9" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 2 1600
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 6
+#   EOS_EXPECTED
+# end
+#
+# describe "main_test10" do
+#   it_behaves_like "test_main", <<~EOS_INPUT.chomp, <<~EOS_EXPECTED.chomp
+# 10 5400
+# 5 1000
+# 3 500
+# 5 1000
+# 3 500
+# 5 1000
+# 3 500
+# 5 1000
+# 3 500
+# 5 1000
+# 3 500
+#   EOS_INPUT
+# 5
+#   EOS_EXPECTED
+# end
