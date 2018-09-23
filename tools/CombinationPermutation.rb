f1547d695717aba77d57493da010a395d99372c5
Leon <leopard10000@gmail.com>
2018-09-23 22:54:34 +0900

    素数列。順列組み合わせ数計算

---
+++ tools/CombinationPermutation.rb
@@ -0,0 +1,14 @@
+class Integer
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
