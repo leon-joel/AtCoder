00265df9aa55d95a75dbd8a52a55842786334d0e
Leon <leopard10000@gmail.com>
2018-12-16 19:42:28 +0900

    ARC 029: 終了後に自力でDもAC

--- AGC029/d.rb
+++ AGC029/d.rb
@@ -3,37 +3,26 @@ require 'pp'
 def gets; ARGF.gets; end
 
 def main
-  # 原点(0, 0)
-  # (j, i) →j,x高橋, ↓i,Y青木 として考える
   h, w, n = gets.split.map(&:to_i)
 
   # 高橋君は動けるときは必ず動かないといけない
 
-  # 障害物がない場合、ans == w となる ※青木は一切動かないので
+  # 障害物がない場合、ans == w となる ※青木は一切動かず壁まで到達
 
-  # x <= y の障害物は無視 ※決してたどり着けないので
+  # x < y の障害物は無視 ※決してたどり着けないので
 
-  # w <= x の障害物も無視 ※これらの障害物に行くよりは外壁に向かったほうがいいので
-  # あ、こんな障害物存在し得ないんだった^^; （原点0,0で考えた場合）
-
-  # その中で最も x が小さいものを見つければよい
-  # ans は【当該障害物の x 座標】となる
-
-  # だめだった！
-
-  # x <= y は無視
-  #
   # x で昇順ソート
   #
   # x - y を diff と定義
-
-  # 最初に diff == 1 となる障害物がゴール
-  # ただし、その前に diff == 0 となる障害物が見つかった場合、
-  # diff += 1 する
+  # min_diff = 1
   #
-  # 以下同じで、diff == 2 となる障害物がゴールだが、
-  # diff == 1 となる障害物が見つかった場合は
-  # 更に diff += 1 する
+  # 最初に 1 <= diff となる障害物がゴール
+  # ただし、その前に diff == 0 となる障害物が見つかった場合、
+  # min_diff を1加算 する
+  #
+  # 以下同じで、min_diff <= diff となる障害物がゴールだが、
+  # diff == min_diff-1 となる障害物が見つかった場合は
+  # 更に min_diff += 1 する
 
   o_ary = []
   n.times do
@@ -53,13 +42,16 @@ def main
 
   diff = 1
   o_ary.each do |(x, y)|
-    if x - y == diff
-      return puts x
-    elsif (x - y) == (diff - 1)
+    if (x - y) == (diff - 1)
+      # 青木くんの行動を制限するブロック
       diff += 1
+
+    elsif diff <= x - y
+      # 目標ブロックに到達
+      return puts x
     end
   end
-  puts w
+  puts h
 end
 
 if __FILE__ == $0
