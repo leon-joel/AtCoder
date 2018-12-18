d767f3e1aa37394bbba01de35ba4a20773566181
Leon <leopard10000@gmail.com>
2018-12-18 23:42:09 +0900

    ABC 114: C: 解説どおりに実装してAC
    やっている事自体はシンプルに再帰で解いているだけ。
    これくらいはサラッとACできるようにならないと次が見えない。

--- 114/c.rb
+++ 114/c.rb
@@ -2,109 +2,33 @@ require 'pp'
 
 def gets; ARGF.gets; end
 
+C753 = ['7', '5', '3'].freeze
+
+def is753?(s)
+  C753.each do |c|
+    return false if !s.include?(c)
+  end
+  true
+end
+
+# 文字列 s で始まり、upper以下の 753数 の数を返す
+# s自身が 753数ならそれも含む
+def dfs(s, upper)
+  cnt = 0
+  return 0 if upper < s.to_i
+
+  cnt += 1 if is753?(s)
+
+  C753.each do |c|
+    cnt += dfs(s + c, upper)
+  end
+  cnt
+end
+
 def main
-  s = gets.chomp
-  s_digit = s.length
-
-  upper = s.to_i
-
-  ans = 0
-  3.upto(s_digit) do |digit|
-    if digit < s_digit
-      ans += count_75s(false, false, false, digit)
-    else
-      # 桁数がMAXの場合
-      ans += create_75s(s, false, false, false, digit)
-    end
-
-  end
-
-  puts ans
+  n = gets.chomp.to_i
+  puts dfs("", n)
 end
-
-def count_75s(u3, u5, u7, digit)
-  if digit == 1
-    case [u3, u5, u7]
-    when [true, true, true]
-      3
-    when [true, true, false], [true, false, true], [false, true, true]
-      1
-    else
-      0
-    end
-  elsif digit == 2
-    case [u3, u5, u7]
-    when [true, true, true]
-      9
-    when [true, true, false], [true, false, true], [false, true, true]
-      4
-    when [true, false, false], [false, true, false], [false, false, true]
-      2
-    else
-      0
-    end
-  else
-    cnt = count_75s(true, u5, u7, digit-1) +
-        count_75s(u3, true, u7, digit-1) +
-        count_75s(u3, u5, true, digit-1)
-    cnt
-  end
-end
-
-def create_75s(s, u3, u5, u7, digit)
-  ux = s[s.length - digit].to_i
-  if digit == 1
-    if !u3
-      if ux < 3
-        0
-      else
-        1
-      end
-    elsif !u5
-      if ux < 5
-        0
-      else
-        1
-      end
-    elsif !u7
-      if ux < 7
-        0
-      else
-        1
-      end
-    else
-      if ux < 3
-        0
-      elsif ux < 5
-        1
-      elsif ux < 7
-        2
-      else
-        3
-      end
-    end
-  else
-    cnt = 0
-    if 7 < ux
-      cnt += count_75s(u3, u5, true, digit-1)
-    elsif 7 == ux
-      cnt += create_75s(s, u3, u5, true, digit-1)
-    end
-    if 5 < ux
-      cnt += count_75s(u3, true, u7, digit-1)
-    elsif 5 == ux
-      cnt += create_75s(s, u3, true, u7, digit-1)
-    end
-    if 3 < ux
-      cnt += count_75s(true, u5, u7, digit-1)
-    elsif 3 == ux
-      cnt += create_75s(s, true, u5, u7, digit-1)
-    end
-    cnt
-  end
-end
-
-
 
 if __FILE__ == $0
   main
