69bebc86c1a6abb88db0c438d4f6736b178dad15
Leon <leopard10000@gmail.com>
2019-09-29 18:30:59 +0900

    第一回日本最強プログラマー学生選手権決勝-A

--- Training/a.rb
+++ Training/a.rb
@@ -2,20 +2,53 @@ require 'pp'
 
 def gets; ARGF.gets; end
 
+class XY
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+  def to_s
+    "(#{@x},#{@y})"
+  end
+  def ==(other)
+    @x == other.x && @y == other.y
+  end
+  def eql?(other)
+    self.==(other)
+  end
+  def +(other)
+    XY.new(@x + other.x, @y + other.y)
+  end
+end
+
 class Solver
 
   def initialize
   end
 
   def main
-    s = gets.chomp
+    n, m = gets.chomp.split.map(&:to_i)
 
-    if s[0] == '9' || s[1] == '9'
-      puts "Yes"
-    else
-      puts "No"
+    ns = gets.chomp.split.map(&:to_i)
+    ms = gets.chomp.split.map(&:to_i)
+
+    ary = Array.new(2 * 1000000)
+    ns.each_with_index do |a, ia|
+      ms.each_with_index do |b, ib|
+        w = a + b
+        if ary[w].nil?
+          ary[w] = XY.new(ia, ib)
+        else
+          r = ary[w]
+          puts "#{r.x} #{r.y} #{ia} #{ib}"
+          return
+        end
+      end
     end
 
+    puts "-1"
   end
 end
 
