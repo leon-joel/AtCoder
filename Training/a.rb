1fe0438b838663f95f30e74c866c3262bfbc6d21
Leon <leopard10000@gmail.com>
2019-09-30 22:09:53 +0900

    ABC072-B

--- Training/a.rb
+++ Training/a.rb
@@ -2,53 +2,21 @@ require 'pp'
 
 def gets; ARGF.gets; end
 
-class XY
-  attr_accessor :x, :y
-
-  def initialize(x, y)
-    @x = x
-    @y = y
-  end
-  def to_s
-    "(#{@x},#{@y})"
-  end
-  def ==(other)
-    @x == other.x && @y == other.y
-  end
-  def eql?(other)
-    self.==(other)
-  end
-  def +(other)
-    XY.new(@x + other.x, @y + other.y)
-  end
-end
-
 class Solver
 
   def initialize
   end
 
   def main
-    n, m = gets.chomp.split.map(&:to_i)
+    s = gets.chomp
 
-    ns = gets.chomp.split.map(&:to_i)
-    ms = gets.chomp.split.map(&:to_i)
+    ans = ""
 
-    ary = Array.new(2 * 1000000)
-    ns.each_with_index do |a, ia|
-      ms.each_with_index do |b, ib|
-        w = a + b
-        if ary[w].nil?
-          ary[w] = XY.new(ia, ib)
-        else
-          r = ary[w]
-          puts "#{r.x} #{r.y} #{ia} #{ib}"
-          return
-        end
-      end
+    s.each_char.with_index do |c, i|
+      ans += c if (i+1).odd?
     end
 
-    puts "-1"
+    puts ans
   end
 end
 
